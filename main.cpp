096c113fcee51b64fe9fe372c1787575439b05fb
yahada <niadzy14@gmail.com>
2025-11-01 13:34:58 +0300

    full matrix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <cstddef>
 
 void destroy(int ** mtx, size_t created) {
   for (size_t i = 0; i < created; i++) {
     delete[] mtx[i];
   }
   delete[] mtx;
+}
+
+void construct(int ** mtx, int init, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      mtx[i][j] = init;      
+    }
+  }
 }
 
 int ** create(size_t rows, size_t cols) {
@@ -22,13 +31,27 @@ int ** create(size_t rows, size_t cols) {
 }
 
 int main() {
+  size_t r = 0, c = 0;
+  std::cin >> r >> c;
+  if (!std::cin) {
+    std::cerr << "Bad input" << "\n";
+    return 2;
+  }
   int ** mtx = nullptr;
   try {
-    mtx = create(5,5);
+    mtx = create(r, c);
   }
   catch(const std::bad_alloc & e) {
     std::cerr << e.what() << '\n';
     return 1;
   }
-  destroy(mtx, 5);
+  std::cout << "created\n";
+  construct(mtx, 2, r, c);
+  std::cout << mtx[0][0] << "\n";
+  destroy(mtx, r);
 }
+
+
+
+
+
