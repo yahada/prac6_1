c58953edfd1812d7d95851cc4da43f678835efea
yahada <niadzy14@gmail.com>
2025-11-01 14:02:43 +0300

    your input and output

--- main.cpp
+++ main.cpp
@@ -30,6 +30,27 @@ int ** create(size_t rows, size_t cols) {
   return mtx;
 }
 
+void input(int ** mtx, size_t rows, size_t cols) {
+
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      std::cout << "enter " << i << ":" << j << ' ';
+      std::cin >> mtx[i][j];
+    }
+  }
+}
+
+
+void output(int ** mtx, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; ++i) {
+    std::cout << mtx[i][0];
+    for (size_t j = 1; j < cols; ++j) {
+      std::cout << ' ' << mtx[i][j];
+    }
+    std::cout << "\n";
+  }
+}
+
 int main() {
   size_t r = 0, c = 0;
   std::cin >> r >> c;
@@ -46,8 +67,16 @@ int main() {
     return 1;
   }
   std::cout << "created\n";
-  construct(mtx, 2, r, c);
-  std::cout << mtx[0][0] << "\n";
+  construct(mtx, 0, r, c);
+  // std::cout << mtx[0][0] << "\n";
+  input(mtx, r, c);
+  if (!std::cin) {
+    std::cerr << "Bad input" << "\n";
+    return 2;
+  }
+
+  output(mtx, r, c);
+  // std::cout << mtx[0][0] << "\n";
   destroy(mtx, r);
 }
 
